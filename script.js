fdc6199b5a1812200cec20b38567b0cef389b9cf
prerana <preranamagar19@gmail.com>
2026-02-17 14:46:47 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,113 @@
+// let user = {
+//    name: "Riya",
+//     age: 22,
+//      city: "Kathmandu"
+
+// for (let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+// let i= 10;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// for (let i=10; i>=1; i--){
+//     console.log(i);
+// }
+
+// let i=10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+
+
+// for (let i=1; i<=20; i++) {
+//     if(i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// let i = 1;
+// while(i<=20){
+//     if (i % 2 === 0){
+//         console.log(i);
+      
+//     }
+//       i++;
+// }
+
+
+// let i = 1;
+// while(i<=15){
+//     if(i % 2 === 1){
+//         console.log(i);
+
+//     }
+//     i++;
+// 
+
+
+// for(let i = 1; i<=10; i++){
+
+//         console.log(i * 5);
+//     }
+
+
+// for( let i = 1; i <=10; i++){
+//     console.log(`5 * ${i} = ${5 * i}`);
+// }
+
+// let i = 1;
+// while(i <= 10){
+//     console.log(`5* ${i} = ${5 * i}`);
+// }
+
+
+// qn6
+
+// for(let i=1; i<=100; i++){
+//     console   
+// }
+
+
+
+// let name="samipab";
+// console.log(name);
+
+// let a = 6;
+// let b = 5;
+// sum = a + b;
+// console.log(sum);
+
+
+// let num = 32;
+// if(num % 2 === 0){
+//     console.log(" even");
+//  } else {
+//     console.log("odd ");
+
+// }
+
+
+// to print the number from 1 to 10.
+
+// for(let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+
+// for (let i = 10; i >=1; i--){
+//     console.log(i)
+// }
+
+for (let i = 1; i <=20; i++){
+    if( i % 2 === 1){
+        console.log(i);
+    }
+}
