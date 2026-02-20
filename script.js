b9b90b7938f1863a808b72a62d81c8bf1bd7dc05
prerana <preranamagar19@gmail.com>
2026-02-20 21:44:06 +0545

    Add the function practice updates

--- script.js
+++ script.js
@@ -1,174 +1,50 @@
-// let user = {
-//    name: "Riya",
-//     age: 22,
-//      city: "Kathmandu"
+// this is the hoisting concept in the function decleratiion/statement
+// parrot();
 
-// for (let i = 1; i<=10; i++){
-//     console.log(i);
-// }
 
-// let i= 10;
-// while(i<=10){
-//     console.log(i);
-//     i++;
+
+
+
+// function parrot(){
+//     console.log("samipab fav bird")
 // }
 
 
-// for (let i=10; i>=1; i--){
-//     console.log(i);
-// }
-
-// let i=10;
-// while(i>=1){
-//     console.log(i);
-//     i--;
-// }
+// fun expression in the hoisting method
+// parrot();
 
 
 
-// for (let i=1; i<=20; i++) {
-//     if(i % 2 === 0) {
-//         console.log(i);
-//     }
+// let parrot= function(){
+//     console.log("my fav bird")
 // }
 
 
-// let i = 1;
-// while(i<=20){
-//     if (i % 2 === 0){
-//         console.log(i);
-      
-//     }
-//       i++;
+// qn. convert this into the arrow fun
+// function multiply(a,b){
+//     return a * b;
 // }
 
 
-// let i = 1;
-// while(i<=15){
-//     if(i % 2 === 1){
-//         console.log(i);
+// let multiply = (a,b) =>{
+//     return a * b;
+// };
 
-//     }
-//     i++;
-// 
-
-
-// for(let i = 1; i<=10; i++){
-
-//         console.log(i * 5);
-//     }
-
-
-// for( let i = 1; i <=10; i++){
-//     console.log(`5 * ${i} = ${5 * i}`);
-// }
-
-// let i = 1;
-// while(i <= 10){
-//     console.log(`5* ${i} = ${5 * i}`);
-// }
-
-
-// qn6
-
-// for(let i=1; i<=100; i++){
-//     console   
-// }
-
-
-
-// let name="samipab";
-// console.log(name);
-
-// let a = 6;
-// let b = 5;
-// sum = a + b;
-// console.log(sum);
-
-
-// let num = 32;
-// if(num % 2 === 0){
-//     console.log(" even");
-//  } else {
-//     console.log("odd ");
+// function abcd( a,b,c,...val){
+//     console.log(val);
 
 // }
+// abcd(1,2,3,4,5,6);
 
 
-// to print the number from 1 to 10.
-
-// for(let i = 1; i<=10; i++){
-//     console.log(i);
-// }
-
-
-// for (let i = 10; i >=1; i--){
-//     console.log(i)
-// }
-
-// qn6
-// let sum = 0;
-
-// for (let i = 1; i <= 100; i++) {
-//     sum = sum + i;
-// }
-// console.log(sum);
-
-
-// qn7
-// for(let i =1; i <=50; i++) {
-//     if(i%3 === 0){
-//     console.log(i);
-// }                                 
-// }
-
-// qn8
-// let val = prompt ("Give a number")
-
-// for(let i=1; i<=val; i++){
-//     if(i%2 ===0){
-//         console.log(`${i} is even`)
-//     }else{
-//         console.log(`${i} is odd`)
-//     }
-// }
-
-
-// qn9
-//  for (let i=1; i<=100; i++){
-//     if(i%3 ===0 && i%5 ===0){
-//         console.log(i);
-//     }
-//  }
-
-// break and continue 
-// for(let i=1; i<=100; i++){
-//         console.log(i)
-
-//     if(i % 7 ===0){
-//         break;
-//     }
+// function rest(...scores){
+//     let total= 0;
     
+//     scores.forEach(function(val){
+//         total = total + val;
+//     });
+   
+// return total;
 // }
 
-// qn2
-// for(let i=1; i<=20; i++){
-//     if(i%3 ===0){
-//         continue
-//     }
-//     console.log(i);
-// }
-
-// let count =0;
-// for(i=1; i<=100; i++){
-//     if(i%2 ===1)  {
-//         count ++;
-//         console.log(i);
-//     }
-//     if(count ===5) break;
-
-// }
-
-
-// FUNCTION
-
+// console.log(rest(12,34,54,36,43));
