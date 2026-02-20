155049a7f263f03a7988487838a81e430b6e813d
prerana <preranamagar19@gmail.com>
2026-02-21 00:49:11 +0545

    basic operators practice

--- script.js
+++ script.js
@@ -48,3 +48,131 @@
 // }
 
 // console.log(rest(12,34,54,36,43));
+
+
+// var a=23;
+// console.log(a);
+
+// if (true) {
+//   var age = 25;
+// }
+
+// console.log(age);
+
+// console.log(b);
+// var b = 12;
+
+
+// console.log(b);
+// const b = 12;
+ 
+// console.log(b);
+// let b = 12;
+
+// var x = 10;
+// var x = 20;
+// console.log(x);
+
+// let y = 10;
+// let y = 20;
+// console.log(y);
+
+// if (true) {
+//   var age = 25;
+// }
+// console.log(age);
+
+// if (true) {
+//   let city = "Delhi";
+// }
+// console.log(city);
+
+
+// const user = { name: "Aman" };
+// user.name = "Rohit";
+// console.log(user.name);
+
+
+
+
+// if (true) {
+//   let city = "Delhi";
+// console.log(city);
+// }
+
+
+
+//  OPERATORS
+
+// let a = 8
+// let b = 3
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+
+// let x = 5;
+
+// x+=3;
+// console.log("After adding 3:", x);
+
+// x-=4
+// console.log("After subtracting 4:", x);
+
+// x *= 2;
+// console.log("Multiply by 3:", x);
+
+
+// LOGICAL OPERATOR
+//  let a= true;
+//  let b= false;
+
+//  console.log(a && b);
+//  console.log(a || b);
+//  console.log(!a);
+//  console.log(!b);
+
+//COMPARISION OPERATOR
+// console.log(10>4);
+// console.log(7<3);
+// console.log(4>4);
+// console.log(1>4);
+// console.log(6>4);
+
+// let num1 = 15;
+// let num2 = 4;
+
+// if(num1%num2 === 0){
+//     console.log("divisible");
+// }else{
+//         console.log("NOt divisible");
+
+
+// }
+
+
+//ARTHMETIC OPERATOR
+// let num1 = 12;
+// let num2 = 5;
+
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1%num2);
+
+
+// Assignment operator 
+
+// let x = 10;
+
+// x += 7;
+// console.log("add by 7:", x)
+
+// x /= 3;
+// console.log("divided by 3:", x)
+
+// x *= 4;
+// console.log("multiply by 4:", x)
