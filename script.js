460bfccf42eefa9fb4fb09725a877e394d5eb4d9
prerana <preranamagar19@gmail.com>
2026-02-21 16:35:31 +0545

    just add css

--- script.js
+++ script.js
@@ -1,98 +1,122 @@
-// let x = 8;
-// let y = 3;
+// data types practice
 
-// x+=2;
-// console.log(x);
+// let a = 10;
+// let b = "10";
+// let c = true;
+// let d = null;
+// let e;
+// let f = undefined;
 
-// y*=4;
-// console.log(y);
-
-// if(x % y === 0){
-//     console.log("divisible");
-// } else {
-//     console.log(" Not divisible");
-    
-// }
- 
-// console.log( x > y);
-
-
-// let a = 25;
-// let b = 6;
-
-// console.log(a/b);
-// console.log(a%b);
-// console.log(a*b);
-// console.log(a-b);
-
-// let num = 17;
-//  if (num % 2 === 0) {
-//  console.log("its a odd number");
-//  } else {
-//     console.log("its a odd number");
-//  }
-
-// let num = 30;
-// if(3%30 === 0 && 4%30 === 0){
-// console.log("divisible by both")
-// } else {
-//     console.log("Not divisible by both")
-// }
-
-// let x = 12;
-// let y = 20;
-
-// console.log(x < y);
-// console.log(x == y);
-// console.log(x != y);
-
-// let age = 8;
-// if (age<=18){
-//     console.log("adult")
-// }else {
-//     console.log("minor")
-// }
-
-// let num = 45;
-// if(num > 10 && num <20){
-// console.log("Number is between 10 and 20");
-// } else {
-//     console.log("Number is Not between 10 and 20");
-// }
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+// console.log(typeof f);
 
 // let x = 5;
-// let y = 10;
+// let y = "5";
 
-// x+= 5;
-// console.log(x);
-
-// console.log(5 == "5"); true
-// console.log(5 === "5"); false
+// console.log(x + y);  due to concatination in js but i n term of substract it does not work
+// console.log(x - y);
 
 
+// console.log(Boolean(0)); false
+// console.log(Boolean(1)); true
+// console.log(Boolean("")); false
+// console.log(Boolean("Hello"));  true
 
-// let marks = 98;
+// console.log(typeof null)  
+
+// console.log("5" + 2);
+// console.log("5" - 2);
+// console.log(true + 1);
+// console.log(false + 1);
+
+
+
+//loop question practice
+
+// Use a for loop to print numbers from 1 to 10.
+// for(let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+// Use a for loop to print numbers from 10 down to 1.
+// for( let i=10; i>=1; i--){
+//     console.log(i);
+// }
+
+// Use a for loop and if statement to print only even numbers.
+
+// for (let i = 1; i<=20; i++){
+//     if(i%2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// Use a for loop to calculate the sum of numbers from 1 to 10.
+
+// let sum=0;
+// for( let i=1; i<=10; i++){
+//     if(sum = sum+ i){
+//         console.log(sum);
+//     }
+// }
+
+
+// Use a for loop to calculate factorial of 5.
+// let num = 5;
+// let fact = 1;
+
+// for(let i=1; i<=num; i++){
+//     fact*=i;
+// }
+//     console.log(fact);
+
+
+
+// this is for multiplication
+// for(let i=1; i<=10; i++){
+//         console.log(5*i)
+    
+// }
+
+// let n = 5;
+
+// for (let i = 1; i <= n; i++) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+// 🟢 Question 7 – Multiplication Table (num = 7)
+
+// for(let i=1; i<=10; i++){
+//     console.log(7*i)
+// }
+
+// for(let i=1; i<=10; i++){
+//     console.log(`7*${i} = ${7*i}`);
+// }
+
+// while loop
  
-// if ( marks >=90){
-//     console.log("A")
-// } else if (marks >= 75){
-//     console.log("B")
-// } else {
-//     console.log("C")
+// let i= 1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
 // }
 
 
+// let sum = 0;
 
-
-// If divisible by 2 → print "Fizz"
-// If divisible by 3 → print "Buzz"
-// If divisible by both → print "FizzBuzz"
-// let num = 1;
-
-// if(num %2 === 0){
-//     console.log("Fizz");
-// } else if (num%3 === 0){
-//     console.log("Buzz");
-// } else{
-//     console.log("FIzBuzz");
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 !== 0) { // odd numbers
+//         sum = sum + i;
+//     }
 // }
+
+// console.log(sum);
