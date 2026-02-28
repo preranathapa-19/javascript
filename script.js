95ace9f8c6af714b1ea6dbdcae3db852d6663fe8
prerana <preranamagar19@gmail.com>
2026-02-28 22:32:51 +0545

    funcion-basic

--- script.js
+++ script.js
@@ -28,12 +28,115 @@
 // console.log(result);
 
 
-function checkEvenOdd(num) {
-  if (num % 2 === 0) {
-    return "Even";
-  } else {
-    return "Odd";
-  }
-}
+// function checkEvenOdd(num) {
+//   if (num % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// }
 
-console.log(checkEvenOdd(7));
+// console.log(checkEvenOdd(7));   
+
+
+// write a BMI calculator 
+
+// function bmi(weight, height){
+//   return weight/ (height * height);
+
+// }
+//  console .log(bmi(69,1.7).toFixed(2));
+
+
+// create a reuseable  discount calculator (HOf )
+
+// function discountCalculator(discount){
+//   return function(price){
+//     return price - price * (discount / 100);
+//   };
+// }
+
+// let ten = discount = discountCalculator(10);
+// let twenty = discount = discountCalculator(20);
+
+
+// console.log(ten(1200));
+// console.log(twenty(2930));
+
+
+// Create a function isEven that returns true if number is even, otherwise false
+
+// function isEven(num){
+//   return num% 2 ===0;
+// }
+//  console.log(isEven(3));
+
+//  Create a function findMax that takes 3 numbers and returns the largest
+
+// function findMax(a,b,c){
+//   if(a>=b && a>=c){
+//     return a;
+//   }else if(b>=a && b>=c){
+//     return b;
+//   }else{
+//     return c;
+//   }
+// }
+
+// console.log(findMax(23,53,21));
+ 
+
+// function hi(){
+//   console.log("hi, i am learning function")
+// }
+// hi();
+
+
+
+// : Print the number passed as parameter
+// function peru(num){
+//   console.log(num);
+// }
+// peru(12);
+
+
+// function printNumber(num) {
+//   console.log(num);
+// }
+
+// printNumber(10);
+
+// Return sum of two numbers.
+// function sum(a,b){
+//   return a + b;
+// }
+
+// console.log(sum(8,9))
+
+// Return difference of two numbers
+// function diff(a,b){
+//   return a - b;
+
+// }
+
+// console.log(diff(7,3));
+
+// Return double of number.  it means that the multiply of that number byu 2
+// function doubleNumber(num){
+//   return num * 2;
+
+// }
+// console.log(doubleNumber(7));
+
+// Return "Adult" or "Minor"
+
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return "Adult";
+//   } else {
+//     return "Minor";
+//   }
+// }
+
+// console.log(checkAge(20));
+// console.log(checkAge(15));
