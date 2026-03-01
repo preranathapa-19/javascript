f4bdce909a0a6a79a7bb94dab7f354174f086592
prerana <preranamagar19@gmail.com>
2026-03-01 11:03:33 +0545

    few more function

--- script.js
+++ script.js
@@ -1,142 +1,51 @@
-// function
-
-//  Q1. Create a function that prints "Hello World"
-
-// function sayhello() {
-//     console.log("HelloWord")
-// }
-// sayhello();
-
-
-//  Q1. Create a function that prints "Hello World"
-
-// function great(name){
-//     console.log("Hello," +name);
-
-// }
-
-// great("Samipab");
-
-
-// Q3. Create a function that takes two numbers and returns their sum.
-
-// function add(a,b) {
-//     return a+b;
-// }
-
-// let result = add(2,8);
-// console.log(result);
-
-
-// function checkEvenOdd(num) {
-//   if (num % 2 === 0) {
-//     return "Even";
-//   } else {
-//     return "Odd";
-//   }
-// }
-
-// console.log(checkEvenOdd(7));   
-
-
-// write a BMI calculator 
-
-// function bmi(weight, height){
-//   return weight/ (height * height);
-
-// }
-//  console .log(bmi(69,1.7).toFixed(2));
-
-
-// create a reuseable  discount calculator (HOf )
-
-// function discountCalculator(discount){
-//   return function(price){
-//     return price - price * (discount / 100);
-//   };
-// }
-
-// let ten = discount = discountCalculator(10);
-// let twenty = discount = discountCalculator(20);
-
-
-// console.log(ten(1200));
-// console.log(twenty(2930));
-
-
-// Create a function isEven that returns true if number is even, otherwise false
-
-// function isEven(num){
-//   return num% 2 ===0;
-// }
-//  console.log(isEven(3));
-
-//  Create a function findMax that takes 3 numbers and returns the largest
-
-// function findMax(a,b,c){
-//   if(a>=b && a>=c){
-//     return a;
-//   }else if(b>=a && b>=c){
-//     return b;
-//   }else{
-//     return c;
-//   }
-// }
-
-// console.log(findMax(23,53,21));
- 
-
-// function hi(){
-//   console.log("hi, i am learning function")
-// }
-// hi();
-
-
-
-// : Print the number passed as parameter
-// function peru(num){
-//   console.log(num);
-// }
-// peru(12);
-
-
-// function printNumber(num) {
-//   console.log(num);
-// }
-
-// printNumber(10);
-
-// Return sum of two numbers.
-// function sum(a,b){
-//   return a + b;
-// }
-
-// console.log(sum(8,9))
-
-// Return difference of two numbers
-// function diff(a,b){
-//   return a - b;
-
-// }
-
-// console.log(diff(7,3));
-
-// Return double of number.  it means that the multiply of that number byu 2
-// function doubleNumber(num){
-//   return num * 2;
-
-// }
-// console.log(doubleNumber(7));
-
 // Return "Adult" or "Minor"
+// function checkage(age){
+//     if (age>=18){
+//         return"Adult";
+//     } else{
+//         return "Minor";
+//     }
+// }
+// console.log(checkage(32));
+// console.log(checkage(12));
+// console.log(checkage(52));
+// console.log(checkage(2));
 
-// function checkAge(age) {
-//   if (age >= 18) {
-//     return "Adult";
-//   } else {
-//     return "Minor";
-//   }
+// Return first character of string
+// function getfirstchar(str){
+//     return str[0];
+// }
+// console.log(getfirstchar("samipabRokaMagar"));
+// console.log(getfirstchar("RokaMagar"));
+// console.log(getfirstchar("Magar"));
+// console.log(getfirstchar("okaMagar"));
+
+
+// Return last character of string.
+// function getlastchar(str){
+//     return str[str.length -1];
 // }
 
-// console.log(checkAge(20));
-// console.log(checkAge(15));
+// console.log(getlastchar("Po"));
+// console.log(getlastchar("ergdk"));
+// console.log(getlastchar("okh"));
+// console.log(getlastchar("Pokhar"));
+
+// Return square of number
+// function square(num){
+//     return num * num;
+// }
+// console.log(square(5));
+// console.log(square(2));
+// console.log(square(6));
+// console.log(square(9));
+
+// Return true if first number is greater
+// function isGreater(a,b){
+//     return a>b;
+
+// }
+// console.log(isGreater(23,21));
+// console.log(isGreater(23,21));
+// console.log(isGreater(20,21));
+// console.log(isGreater(6,2));
