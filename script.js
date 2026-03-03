3d1349cd8055470261ed2ca0554ecb2e84c992ca
prerana <preranamagar19@gmail.com>
2026-03-03 09:55:23 +0545

    complte the full array

--- script.js
+++ script.js
@@ -125,4 +125,120 @@
 // let arr = [1,2,3,4,5,6];
 // let arr2 = [...arr]
 
+// practice question of array  
 
+// create an array with 3 fruits and print the second one 
+// let fruits = ["apple", "banana","orange", "kiwi"];  ==fruits[1]
+
+// add the "mango at the end and "pinaapple" at the beginning of the aarray;
+// let fruits = ["banana","orange", "kiwi"]
+// fruits.unshift("pinaapple");
+// fruits.push("Mango");
+
+
+// replace banana with thr kiwi in the array above 
+
+// let fruits = ["banana","orange", "apple"];
+// fruits.shift();
+// fruits.unshift("kiwi");
+
+
+// whats the difference between the push and unshift
+// push = add the value in the last
+// unshift = add the value at first
+
+// shift = remove first
+// pop = remove last
+
+// remove the last item
+// let num = [1,2,3,4,5]
+// num.pop();
+
+// remove first
+// let num = [1,2,3,4,5]
+// num.shift();
+
+//Insert red and blue at index 1 in this array
+// let colors = ["Green", "Yellow"];
+// colors.splice(1,0,"Red", "Blue");
+
+//extract only the middle 3 elemnets from this array
+//  let items =[1,2,3,4,5,6];
+// let newarr=items.slice(1,4);
+// console.log(newarr);
+
+
+//  items.reverse();
+
+// sort this array alphabetically and then reverse it 
+
+// let names = ["zara", "peru", "samip", "mira"];
+// names.sort().reverse();
+
+// use .map() to square  eacch numbers 
+// let arr =[1,2,3,4];
+// let newarr = arr.map(function(val) {
+//     return val * val;
+// });
+
+// use .filter() to keep numbers greater than 10.
+// let arr = [5,12,67,30,6,2];
+// let newarr= arr.filter(function(val){
+//     return val>10;
+// });
+
+// anotheer way like fat arrow function 
+// let arr = [12,24,3,68,9,2,1];
+// let newarr = arr.filter((val) => {
+//     return val>10;
+// });
+
+//use .reduce
+// let arr = [10,20,30];
+// let sum= arr.reduce(function(acc , val){
+//     return acc+val;
+
+// },0);
+
+//use .find
+// let arr= [12,15,3,5,8,20];
+// let newarr= arr.find(function(val){
+//     return val<10;
+// })
+
+// let arr= [12,15,3,5,8,20];
+// let newarr= arr.find((val) =>{
+//     return val<10;
+// })
+
+// use .some 
+// let arr = [12,34,64,,11,24,25];
+// let newarr= arr.some((val) =>{
+//     return val<35;
+// })
+
+// .ever that all the numbers are even or not
+// let num =[22,13,15,67,44,85];
+// let newnum= num.every((val) =>{
+//     return val %2 === 0;
+// });
+
+// Destructure this array to get firtname anad lastname 
+// let fullname= ["samip", "roka"];
+// let[firstname, lastname] =fullname;
+
+//merge two array using the spread operators
+
+// let peru =["preety", "lovely"];
+// let samip =["cute", "loveable"];
+
+// let togther =[...peru,...samip];
+
+// let coun = ["nepal", "japan"];
+// coun = ["india", ...coun];
+// coun = [...coun,"Korea"];
+
+
+//clone this arary
+// let arr = [1,2,3];
+// let arr2=[...arr];
