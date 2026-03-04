9eb58ae641dc6f07cebd68f501b4666cfca7dde3
prerana <preranamagar19@gmail.com>
2026-03-04 22:01:27 +0545

    mini project- student managemnet system

--- script.js
+++ script.js
@@ -1,168 +1,58 @@
-//practice question from the array and the object
-
-// Create an array of 5 numbers. Print the sum of all numbers
-
-// const numbers =[23,44,52,43,22];
-// let sum = 0;
-// for(let number of numbers){
-//     sum +=number;
-// }
-// console.log(sum);
+// 1️ Print all student names
+// 2️ Print students who passed (>= 70)
+// 3️ Find highest marks
+// 4️ Find average marks
 
 
-//create an object
+const students = [
 
-// const student ={
-//     name:"samipab",
-//     roll: 1,
-//     marks: 44
+  {name: "Asha", marks: 80},
+  {name: "Rohan", marks: 65},
+  {name: "Mina", marks: 90},
+  {name: "Sita", marks: 55}
+];
 
-// };
-// console.log(student.marks);
-
-//create an array of students
-
-// const students=[
-//  {name: "samipab", marks: 80},
-//  {name: "peru", marks: 60},
-//  {name: "prerana", marks: 90}
-// ];
-
+// 1
+// console.log(" All students");
 // for(let student of students){
-// if(student.marks >70){
-//     console.log(student.name);
-// }
+//     concole.log(student.name);
 // }
 
-// Find the highest marks from the above array.
-// const students=[
-//  {name: "samipab", marks: 80},
-//  {name: "peru", marks: 60},
-//  {name: "prerana", marks: 90}
-// ];
-
-// let highest= students[0].marks;
-
-// for(let i= 0; i<students.length; i++){
-//     if(students[i].marks>highest){
-//         highest = students[i].marks;
+// 2
+// for(let student of students){
+//     if(student.marks>=70){
+//         console.log(student.name);
 //     }
 // }
 
-// console.log(highest);
-
-
-// const students=[
-//  {name: "samipab", marks: 80},
-//  {name: "peru", marks: 60},
-//  {name: "prerana", marks: 90}
-// ];
-
-// let highest = 0;
-
-// for(let high of students){
-//     if(high.marks>highest){
-//        highest = high.marks;
+//3
+// let highest = students[0].marks;
+// for(let student of students){
+//     if(student.marks>highest){
+//         highest = student.marks;
 //     }
 // }
 // console.log("highest marks:", highest);
 
-
-
-// const cart=[
-//  {item: "Shoes", price: 2000},
-//  {item: "Bag", price: 1500},
-//  {item: "Watch", price: 3000}
-// ];
-
-// let sum = 0;
-
-// for (let item of cart){
-//     sum+=cart.price;
-// }
-// console.log(sum);
-
-// Count how many products cost more than 2000.
-// const cart = [
-//     {item: "Shoes", price: 2000},
-//     {item: "Bag", price: 1500},
-//     {item: "Watch", price: 3000}
-// ];
-// let count = 0;
-
-//  for(let product of cart){
-//     if(product.price>2000){
-//         count++;
-//     }
-//  }
-//  console.log(count);
-
-
-// Find Average Marks
-// const students = [
-//   {name: "Asha", marks: 80},
-//   {name: "Rohan", marks: 70},
-//   {name: "Mina", marks: 90}
-// ];
-
+//4
 // let total = 0;
 
 // for(let student of students){
-//     total+=student.marks;
-
+//     total+= student.marks;
 // }
 
 // let average = total / students.length;
 
-// console.log("Average MArks:", average);
+// console.log("Average Marks:", average);
 
 
-// Find Average Marks  passing marks = 75
-// const students = [
-//   {name: "Asha", marks: 80},
-//   {name: "Rohan", marks: 70},
-//   {name: "Mina", marks: 90}
-// ];
-// let count = 0;
 
-// for (let student of students){
-//     if(student.marks>75);
-//     count++;
-// }
-// console.log(count);
+// Count how many students failed (< 70)
 
-
-// 🟢 QUESTION 3 – Find Cheapest Product
-// const products = [
-//   {item: "Shoes", price: 2000},
-//   {item: "Bag", price: 1500},
-//   {item: "Watch", price: 3000}
-// ];
-
-// let cheapest =products[0];
-
-// for (let product of products){
-//     if(product.price<cheapest.price){
-//         cheapest=product;
-//     }
-// }
-
-// console.log("Cheapest price:", cheapest.item);
-
-//price range between 1500 n 2500
-const products = [
-  {item: "Shoes", price: 2000},
-  {item: "Bag", price: 1500},
-  {item: "Watch", price: 3000}
-];
-
-let cheapest =products[0];
-
-for (let product of products){
- 
-   if(product.price>1500 && product.price<2500){
-console.log(product.item);
-
-   }
-
+let failcount =0;
+for(let student of students){
+    if(student.marks<70){
+    failcount++;
 }
+}
+console.log("Student Fail:", failcount);
