9687c237d49e348f0a821e9a561ac3a59532c870
prerana <preranamagar19@gmail.com>
2026-03-06 19:20:24 +0545

    array question practice please god thankyou

--- script.js
+++ script.js
@@ -27,12 +27,62 @@
 // console.log("largest number:", largest)
 
 // Count how many even numbers are in the array.
-const numbers = [1,2,3,4,5,6,7,8];
+// const numbers = [1,2,3,4,5,6,7,8];
 
-let Count = 0;
-for(let num of numbers){
-    if(num % 2 === 0){
-        Count++;
-    }
-}
-console.log("even number:", Count);
+// let Count = 0;
+// for(let num of numbers){
+//     if(num % 2 === 0){
+//         Count++;
+//     }
+// }
+// console.log("even number:", Count);
+
+// Print the last number from the array
+// const numbers = [10, 20, 30, 40, 50];
+// console.log(numbers[numbers.length -1]);
+
+// Find the product (multiplication) of all numbers.
+// const nums = [2, 3, 4];
+// let product = 1;
+
+// for(let num of nums){
+//     product*=num;
+// }
+
+// console.log(product);
+
+
+// 4 – Print Only Odd Numbers
+
+// const numbers = [1,2,3,4,5,6,7,8];
+
+// for(let number of numbers){
+//     if(number%2 ===1){
+//          console.log(number);
+//     }
+//  }
+
+
+// const nums = [25, 10, 40, 5, 30];
+
+// let smallest =nums[0];
+
+// for(let num of nums){
+//     if(num<smallest){
+//         smallest = num;
+//     }
+// }
+// console.log("smallest number:", smallest);
+
+// Count Numbers Greater Than 50
+// const scores = [30, 60, 80, 40, 90];
+//  let count = 0;
+
+//  for(let score of scores){
+//     if(score <50){
+//         count++;
+//     }
+//  }
+
+//  console.log("count:", count);
+
