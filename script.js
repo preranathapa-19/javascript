1ba2109b7ca717576be8b2bc2f56accaf136bd37
prerana <preranamagar19@gmail.com>
2026-03-22 19:37:06 +0545

    Student Result System

--- script.js
+++ script.js
@@ -1,97 +1,37 @@
-// 1. Student Result System
-//  Concept: Arrays + Objects + Loop + Condition
-//  Create a system to:
-// Store students (name + marks)
-// You need to:
-// Print topper name
-// Count how many passed (>=50)
-// Count how many failed
-// Find average marks
+// Shopping Cart Calculator
+// Concept: Objects + Loop + Total Calculation
+// Create a cart system
+// ;
+// 👉 You need to:
+// Calculate total price 💰
+// Find most expensive item 🏷️
+// Print items above 2000
 
-let students =[
-  {name: "Ram", marks: 80},
-  {name: "Simip", marks: 45},
-  {name: "Harn", marks: 20},
-  {name: "Gilli", marks: 90},
-  {name: "Gitam", marks: 30},
-  {name: "peru", marks: 92},
-  {name: "samipab", marks: 99}
+let cart = [
+  {item: "Shoes", price: 2000, quantity: 2},
+  {item: "Bag", price: 1500, quantity: 1},
+  {item: "Watch", price: 3000, quantity: 1}
 ];
 
-// Find:
-// topper 🏆
-// average marks 📊
-// number of passed/failed students
 
-// let topper = students[0];
-
-// for(let top of students){
-//     if(top.marks>topper.marks){
-//         topper = top;
-//     }
-// }
-//     console.log("topper:", topper.name);
-
-
+// Calculate total price 💰
 let total = 0;
-
-for(let avg of students){
-  total += avg.marks;
+for (let carts of cart){
+    total+= carts.price * carts.quantity;
+        total++;
+    
 }
+console.log("Total price:", total);
 
-let average = total / students.length;
-console.log("Average Marks:", average);
+// Find most expensive item 🏷️
+let expensive = cart[0];
 
-let passCount = 0;
-let failCount =0;
-
-for(let student of students){
-    if(student.marks>=40){
-        console.log(student.name, "pass");
-        passCount++;
-
-        }else{
-            console.log(student.name, "fail")
-                failCount++;
-            
-        }
-}
-
-
-//print the topper name
-
-let topper = students[0];
-
-for(let student of students){
-    if(student.marks>topper.marks){
-        topper = student;
-    }
-}
-console.log("Topper is:", topper.name);
-
-
-// Count how many passed (>=50)
-
-let passCounts = 0;
-
-for (let student of students){
-    if(student.marks>=50){
-        console.log("pass");
-        passCounts++;
+for(let carts of cart){
+    if(carts.price= carts.quantity > expensive.price* expensive.quantity){
+        expensive= carts;
     }
 }
 
-console.log("Total passes:", passCounts);
+console.log("Most Expensive:", expensive);
 
-// Count how many fail (<50)
 
-let failcounts = 0;
-
-for(let student of students){
-    if(student.marks<50){
-        console.log(student.name, "fail");
-        failcounts++;
-    }
-}
-
-console.log("Total fail:", failcounts);
